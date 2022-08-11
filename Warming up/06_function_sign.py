2faafff35c1fe3e613124268d370fddcfe9665e7
kay0620 <young_0620@naver.com>
2022-08-11 23:38:13 +0900

    Create 06_function_sign.py

---
+++ Warming up/06_function_sign.py
@@ -0,0 +1,8 @@
+#시그니처 함수를 작성
+
+def sign():
+    print("이 프로그램은 Python 실습과정에 의해 만들어졌습니다.")
+    print("블로그: https://blog.naver.com/young_0620")
+    print("이메일 : young_0620@naver.com")
+    
+sign()
