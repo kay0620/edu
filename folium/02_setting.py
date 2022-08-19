6c8316cc3a0ed61baac3137e30e0e555319c7a38
kay0620 <young_0620@naver.com>
2022-08-19 12:49:04 +0900

    Update 02_setting.py

--- folium/02_setting.py
+++ folium/02_setting.py
@@ -2,6 +2,7 @@ import folium
 
 #Zoom 
 #tiles : Stamen Toner, Stamen Terrain
+#size : width, height
 m = folium.Map(location = [35.1795543,129.0756416], 
                zoom_start = 13, 
                tiles="Stamen Toner",
