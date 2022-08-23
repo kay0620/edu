d38562b6b0afd99fb41d1363e915325663f96030
kay0620 <young_0620@naver.com>
2022-08-23 22:28:37 +0900

    Delete Warming up directory

--- Warming up/01_print_subway.py
+++
@@ -1,3 +0,0 @@
-
-station = input('사용자 입력: ')
-print(station + "행 열차가 들어오고 있습니다.")
--- Warming up/05_password.py
+++
@@ -1,9 +0,0 @@
-# 비밀번호 자동 생성
-# 남은 글자 중 처음 세 자리 + 글자 갯수 + 글자 내 'e'의 갯수 + '!'로 구성
-
-url = input('사이트 입력 : ')
-string = url.replace('http://','')
-string = string[:string.index('.')]
-
-new_passwd = string[:3] + str(len(string)) + str(string.count('e')) + '!'
-print("새 비밀번호는 {} 입니다.".format(new_passwd))
