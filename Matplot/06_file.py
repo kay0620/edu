76cd4fb37780e343ada409c7faf2bd52c618d686
kay0620 <young_0620@naver.com>
2023-06-19 14:10:25 +0900

    Delete Matplot directory

--- Matplot/06_file.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = [1, 2, 3]
-y = [2, 4, 8]
-
-plt.plot(x, y)
-
-#dpi : 파일에 저장할 그림의 해상도
-plt.savefig('graph.png', dpi=100) 
--- Matplot/07_text.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = [1, 2, 3]
-y = [2, 4, 8]
-
-plt.plot(x, y, marker='o')
-
-for idx, txt in enumerate(y):
-    plt.text(x[idx], y[idx] + 0.3, txt, ha='center', color='blue')
